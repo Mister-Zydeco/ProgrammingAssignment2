c81cb6032e634e049cc8875229d8e9f2492de2ff
David Nadler <mxyptlx-biz@yahoo.com>
2015-12-14 08:09:36 -0800

    cachematrix.R initial commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates special object holding a numeric matrix (assumed always to
+## be invertible) and its cached inverse. The cached inverse is null
+## until it is computed with the companion function "cacheSolve" below.
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInvers <- NULL
+    set <- function(mxToInvert) {
+       x <<- mxToInvert
+       cachedInvers <<- NULL
+    }
+    get <- function() x
+    setCachedInvers <- function(calculatedInvers) {
+      cachedInvers <<- calculatedInvers
+    }
+    getCachedInvers <- function() cachedInvers
+    list(set=set, get=get,
+      setCachedInvers=setCachedInvers,
+      getCachedInvers=getCachedInvers)
 }
 
 
-## Write a short comment describing this function
+##  cacheSolve takes as argument(s) special objects as created by makeCacheMatrix.
+## The first time that cacheSolve is called, it will calculate and cache the inverse
+## with the object, and then return the inverse. On subsequent calls with the same
+## object as an argument, it will simply return the cached inverse and avoid a
+## potentially expensive matrix inverse calculation.
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix thatis the inverse of 'x'
+  invers <- x$getCachedInvers()
+  print(invers)
+  if (!is.null(invers)) {
+    message("Found and returning cached inverse")
+    return(invers)
+  }
+  mxToInvert <- x$get()
+  invers <- solve(mxToInvert,...)
+  x$setCachedInvers(invers)
+  invers
 }
